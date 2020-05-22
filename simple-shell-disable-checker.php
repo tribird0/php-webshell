0172612169e862d52d83659b44eb8dc815d66f78
tribird0 <60983424+tribird0@users.noreply.github.com>
2020-05-22 16:48:51 +0630

    Create simple-shell-disable-checker.php

---
+++ simple-shell-disable-checker.php
@@ -0,0 +1,7 @@
+<?php
+echo "Backtrick>>".'ls';
+echo "<br>passthru()>>".passthru("ls");
+echo "<br>system()>>".system("ls");
+echo "<br>exec()>>".exec("ls");
+echo "<br>shell_exec()>>".shell_exec("ls");
+?>
